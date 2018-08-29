e06d5a9a3d167d4561a8f40382d631b1f9ce88f1
Ed <uhrgur@gmail.com>
2018-08-29 12:02:16 +0200

    Query getqanadir

--- Pelelicula/src/model/Q.java
+++ Pelelicula/src/model/Q.java
@@ -5,14 +5,24 @@ public class Q {
 	public String getqAnadirPelicula(String titulo, String descripcion, String trailer, int puntuacion, String categoria, int anio, float precio, String portada) {
 		
 		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO peliculas (titulo, descripcion, trailer, puntuacion, categoria, anio, precio, portada) VALUES ('");
+		sb.append("', '");
 		sb.append(titulo);
+		sb.append("', '");
 		sb.append(descripcion);
+		sb.append("', '");
 		sb.append(trailer);
+		sb.append("', ");
 		sb.append(puntuacion);
+		sb.append(", '");
 		sb.append(categoria);
+		sb.append("', ");
 		sb.append(anio);
+		sb.append(", ");
 		sb.append(precio);
+		sb.append(", '");
 		sb.append(portada);
+		sb.append("');");
 		//NO ESTA TERMINADO
 		return sb.toString();
 	}
