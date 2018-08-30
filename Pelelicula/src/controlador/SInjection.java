6109c62e8a5b78d84f922d50c68bd22166d6d769
Ed <uhrgur@gmail.com>
2018-08-30 12:35:56 +0200

    Pruebas de inyeccion sql

---
+++ Pelelicula/src/controlador/SInjection.java
@@ -0,0 +1,63 @@
+package controlador;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+ 
+public class SInjection extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    @Resource(name = "jdbc/testDB")
+    DataSource ds;
+ 
+    public SInjection() {
+        super();
+    }
+ 
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            Connection con = ds.getConnection();
+ 
+            Statement stmt = con.createStatement();
+            String query = "select * from employee";
+            ResultSet rs = stmt.executeQuery(query);
+ 
+            PrintWriter out = response.getWriter();
+            response.setContentType("text/html");
+            out.print("<center><h1>Employee Details</h1></center>");
+            out.print("<html><body>");
+            out.print("<table border=\"1\" cellspacing=10 cellpadding=5>");
+            out.print("<tr><th>Employee ID</th>");
+            out.print("<th>Employee Name</th>");
+            out.print("<th>Salary</th>");
+            out.print("<th>Department</th></tr>");
+ 
+            while (rs.next()) {
+                out.print("<tr>");
+                out.print("<td>" + rs.getInt("emp_id") + "</td>");
+                out.print("<td>" + rs.getString("emp_name") + "</td>");
+                out.print("<td>" + rs.getDouble("salary") + "</td>");
+                out.print("<td>" + rs.getString("dept_name") + "</td>");
+                out.print("</tr>");
+            }
+            out.print("</table></body></html>");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
